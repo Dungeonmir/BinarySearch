a7a5e3ebd207b36222982175368c8ec271a5addb
Dungeonmir <63905738+Dungeonmir@users.noreply.github.com>
2020-05-07 17:26:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] searchArea = new[] { 1, 3, 10, 5, 2, 20, 15, 40, 30, 25 };
+            int low = 0;
+            int high = searchArea.Length;
+
+            int temp;
+            for (int i = 0; i < searchArea.Length - 1; i++)
+            {
+                for (int j = i + 1; j < searchArea.Length; j++)
+                {
+                    if (searchArea[i] > searchArea[j])
+                    {
+                        temp = searchArea[i];
+                        searchArea[i] = searchArea[j];
+                        searchArea[j] = temp;
+                    }
+
+                }
+
+
+            }
+
+        }
+    }
+}
